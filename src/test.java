6e8cf445001aad159cd3d2af41d1cc1bf5ca70b3
奇点 <3099234061@qq.com>
2023-03-14 14:23:28 +0800

    111

---
+++ src/test.java
@@ -0,0 +1,12 @@
+/**
+ * @author 奇点
+ * @ClassName test
+ * @Description TODO
+ * @date 2023/3/14 14:16
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
